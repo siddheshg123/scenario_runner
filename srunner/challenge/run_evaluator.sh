1968dcd9415555e3a5f59ee683cd69f89d51f37d
German Ros <germanros1987@gmail.com>
2019-04-08 19:46:33 -0700

    minor fixes

--- srunner/challenge/run_evaluator.sh
+++ srunner/challenge/run_evaluator.sh
@@ -5,8 +5,8 @@
 
 python ${ROOT_SCENARIO_RUNNER}/srunner/challenge/challenge_evaluator_routes.py \
 --scenarios=${ROOT_SCENARIO_RUNNER}/srunner/challenge/all_towns_traffic_scenarios1_3_4.json \
---repetitions=1 \
 --routes=${ROOT_SCENARIO_RUNNER}/srunner/challenge/routes_devtest.xml \
+--repetitions=1 \
 --debug=0 \
 --agent=${TEAM_AGENT} \
 --config=${TEAM_CONFIG}
