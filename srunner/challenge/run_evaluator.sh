3f7d4b3659aad2c08de73e6b8e22845af0ae916c
German Ros <germanros1987@gmail.com>
2019-04-08 18:28:19 -0700

    Updated: launching script for the challenge

--- srunner/challenge/run_evaluator.sh
+++ srunner/challenge/run_evaluator.sh
@@ -6,6 +6,7 @@
 python ${ROOT_SCENARIO_RUNNER}/srunner/challenge/challenge_evaluator_routes.py \
 --scenarios=${ROOT_SCENARIO_RUNNER}/srunner/challenge/all_towns_traffic_scenarios1_3_4.json \
 --repetitions=1 \
---routes=${ROOT_SCENARIO_RUNNER}/srunner/challenge/routes_training.xml \
+--routes=${ROOT_SCENARIO_RUNNER}/srunner/challenge/routes_devtest.xml \
 --debug=0 \
---agent=${ROOT_SCENARIO_RUNNER}/srunner/challenge/autoagents/HumanAgent.py
+--agent=${TEAM_AGENT} \
+--config=${TEAM_CONFIG}
